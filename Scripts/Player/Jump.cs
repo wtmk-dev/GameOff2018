02a29fdd4de27ed69d133f020e5c239d6c7d9f3f
WTMK-PC\WTMK <wtmk.dev@icloud.com>
2018-11-02 22:27:35 -0700

    player movement. edditor variables to tweek game feel.

---
+++ Scripts/Player/Jump.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+	// public float fallMultiplier = 2.5f;
+	// public float lowJumpMultiplier = 2f;
+
+	// [SerializeField]
+	// private float jumpPower;
+	// private bool isGrouned = false;
+
+	// private Rigidbody2D rig;
+
+	// void Awake(){
+	// 	rig = GetComponent<Rigidbody2D>();
+	// }
+
+	// void OnCollisionEnter2D( Collision2D other ){
+	// 	if( other.gameObject.tag == "Floor" ){
+	// 		isGrouned = true;
+	// 	}
+	// }
+
+
+	// void OnCollisionExit2D( Collision2D other ) {
+	// 	if( other.gameObject.tag == "Floor" ){
+	// 		isGrouned = false;
+	// 	}
+	// }
+
+	// void FixedUpdate() {
+	// 	if( Input.GetButtonDown( "Jump" ) ){
+	// 		Debug.Log( "jump" );
+	// 		rig.velocity = Vector2.up * jumpPower;
+	// 	}
+	// 	GravityJump( rig );
+	// }
+
+	
+}
--- Scripts/Player/Movement.cs
+++ Scripts/Player/Movement.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-
+	
 	[SerializeField]
-	private float speed = .07f;
-	[SerializeField]
-	private float jumpPower;
-
+	private float gravityScale, gravPower = 1f, jumpGrav = -.5f, fallGrav = 15f, jumpMod = 2f, jumpPower = 1f, speed = 5f;
+	private bool isGrounded, isMoveable, isJumping, hasJumped = false;
+	private float jumpTime;
 	private Rigidbody2D rig;
 	private Vector2 movement;
-	private bool isGrouned = false;
-	public bool IsMovable{get;set;}
+
+	void OnCollisionEnter2D( Collision2D other ){
+		if( other.gameObject.tag == "Floor" ){
+			isGrounded = true;
+			isJumping = false;
+			hasJumped = false;
+			jumpTime = 0f;
+			rig.gravityScale = gravityScale;
+		}
+	}
+
+
+	void OnCollisionExit2D( Collision2D other ) {
+		if( other.gameObject.tag == "Floor" ){
+			isGrounded = false;
+		}
+	}
 
 	void Awake(){
 		rig = GetComponent<Rigidbody2D>();
-		IsMovable = true;
+		isMoveable = true;
+		gravityScale = rig.gravityScale;
 	}
 
 	void FixedUpdate(){
@@ -25,48 +40,49 @@ public class Movement : MonoBehaviour {
 		}		
 	}
 
-	void OnCollisionEnter2D( Collision2D other ){
-		if( other.gameObject.tag == "Floor" ){
-			isGrouned = true;
-		}
-	}
-
-
-	void OnCollisionExit2D( Collision2D other ) {
-		if( other.gameObject.tag == "Floor" ){
-			isGrouned = false;
-		}
-	}
-
 	private void CheckInput(){
 		var horizontal = Input.GetAxisRaw( "Horizontal" );
-		var vert = 0f;
-		if( isGrouned && Input.GetKey( KeyCode.Space ) ){
-			vert = jumpPower;
-		}
-		else{
-			vert = 0f;
-		}
-		movement = new Vector2( horizontal, vert );
-		if( IsMovable ){
+		movement = new Vector2( horizontal, 0f );
+		if( isMoveable ){
 			Move( movement * speed );
+			Jump();
 		}
-
-		// if(  ){
-		// 	Debug.Log( "yo" );
-		// 	Jump( jumpHight );
-		// }
-	} 
+	}
 
 	private void Move( Vector2 movement ){
-		rig.velocity = Vector2.zero;
+		rig.velocity = new Vector2( 0f, rig.velocity.y );
 		rig.AddForce( movement, ForceMode2D.Impulse );
 	}
 
-	private void Jump( Vector2 jump ){
-		//rig.velocity = Vector2.zero;
-		float jp = 0f;
-	
+	private void Jump(){
+		if( jumpTime > gravPower ){
+			isJumping = false;
+		}
+
+		if( isJumping ){
+			jumpTime += Time.fixedDeltaTime;
+			rig.gravityScale -= jumpGrav;
+			rig.velocity += new Vector2( rig.velocity.x, jumpPower * jumpMod );
+		}
+		else if( !isJumping && !isGrounded ){
+			rig.gravityScale += fallGrav;
+			// if( rig.gravityScale > gravityScale * .5 ){
+			// 	rig.gravityScale = gravityScale;
+			// }
+		}
+
+		if( Input.GetKeyDown( KeyCode.Space ) ){
+			if( isGrounded && !isJumping ){
+				isJumping = true;
+			}
+		}
 		
+		if( Input.GetKeyUp( KeyCode.Space ) ){
+			isJumping = false;
+		}
+		
+		
+
 	}
+
 }
