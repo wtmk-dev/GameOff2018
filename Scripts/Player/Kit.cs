5601c125f8c210936dcdcf0a4fca13cb9bd27553
WTMK-PC\WTMK <wtmk.dev@icloud.com>
2018-11-05 21:18:42 -0800

    set up player kit, place holder KeyCodes

--- Scripts/Player/Kit.cs
+++ Scripts/Player/Kit.cs
@@ -7,10 +7,18 @@ public class Kit : MonoBehaviour {
 	private bool isActive = true;
 
 	// short range 
+	public GameObject lSword;
+	public GameObject rSword;
+
 	// long range 
-	
+	public GameObject lWhip;
+	public GameObject rWhip;
+	private bool isAttacking;
+
 	// projectile
-	
+	public GameObject shoot;
+	private bool canShoot = true;
+
 	// Shield
 	public GameObject lShield;
 	public GameObject rShield;
@@ -23,7 +31,10 @@ public class Kit : MonoBehaviour {
 
 	void Update(){
 		if( isActive ){
-			DeployShield();		
+			DeployShield();
+			DeployWhip();
+			DeploySword();	
+			DeployShoot();	
 		}
 	}
 
@@ -33,19 +44,17 @@ public class Kit : MonoBehaviour {
 
 	private void DeployShield(){
 		if( !isBlocking ){
-
-			if( Input.GetKeyDown( KeyCode.Mouse0 ) ){
+			if( Input.GetKeyDown( KeyCode.Mouse0 ) && !isAttacking ){
 				lShield.SetActive( true );
 				isBlocking = true;
 			}
 			
-			if( Input.GetKeyDown( KeyCode.Mouse1 ) ){
+			if( Input.GetKeyDown( KeyCode.Mouse1 ) && !isAttacking ){
 				rShield.SetActive( true );
 				isBlocking = true;
 			}
 		}
 		else{
-
 			if( Input.GetKeyUp( KeyCode.Mouse0 ) && !rShield.activeSelf ){
 				lShield.SetActive( false );
 				isBlocking = false;
@@ -56,10 +65,93 @@ public class Kit : MonoBehaviour {
 				isBlocking = false;
 			}
 
+
 		}
 		
 
 	}
+
+	private void DeployWhip(){
+		if( !isAttacking ){
+			if( Input.GetKeyDown( KeyCode.Q ) && !isBlocking ){
+				lWhip.SetActive( true );
+				isAttacking = true;
+			}
+			if( Input.GetKeyDown( KeyCode.E ) && !isBlocking ){
+				rWhip.SetActive( true );
+				isAttacking = true;
+			}
+			
+
+		}
+		else{
+			if( Input.GetKeyUp( KeyCode.Q ) && !rWhip.activeSelf ){
+				lWhip.SetActive( false );
+				isAttacking = false;
+			}
+			if( Input.GetKeyUp( KeyCode.E ) && !lWhip.activeSelf ){
+				rWhip.SetActive( false );
+				isAttacking = false;
+			}
+
+
+		}
+		
+
+	}
+
+	private void DeploySword(){
+		if( !isAttacking ){
+			if( Input.GetKeyDown( KeyCode.Z ) && !isBlocking ){
+				lSword.SetActive( true );
+				isAttacking = true;
+			}
+			if( Input.GetKeyDown( KeyCode.C ) && !isBlocking ){
+				rSword.SetActive( true );
+				isAttacking = true;
+			}
+			
+
+		}
+		else{
+			if( Input.GetKeyUp( KeyCode.Z ) && !rSword.activeSelf && !lWhip.activeSelf && !rWhip.activeSelf ){
+				lSword.SetActive( false );
+				isAttacking = false;
+			}
+			if( Input.GetKeyUp( KeyCode.C ) && !lSword.activeSelf && !lWhip.activeSelf && !rWhip.activeSelf ){
+				rSword.SetActive( false );
+				isAttacking = false;
+			}
+
+
+		}
+		
+
+	}
+
+	private void DeployShoot(){
+		if( canShoot ){
+			if( Input.GetKeyDown( KeyCode.B ) ){
+				shoot.SetActive( true );
+				canShoot = false;
+			}
+		
+		}
+		
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
